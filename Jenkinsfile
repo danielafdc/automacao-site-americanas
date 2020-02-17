a0d59fcd1ad24ba4a7f3f0faf763d4a311ff9b45
Daniela Ferreira <daniela.ferreira@venturus.org.br>
2020-02-16 23:11:43 -0300

    Add Jenkins file and capabilities for headless

---
+++ Jenkinsfile
@@ -0,0 +1,23 @@
+pipeline {
+    agent {
+        docker {
+            image 'ruby'
+            args '--link selenium'
+        }
+    }
+    environment {
+        CI = true
+    }
+    stages {
+        stage('Bundle') {
+            steps {
+                sh "bundle install"
+            }
+        }
+        stage('Run Features') {
+            steps {
+                sh "bundle exec cucumber -p ci"
+            }
+        }
+    }
+}
--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,26 @@ require "capybara"
 require "capybara/cucumber"
 require "selenium-webdriver"
 
+@browser = ENV['BROWSER']
+
+if @browser.eql?('headless')
+    Capybara.javascript_driver = :selenium
+    Capybara.run_server = false
+
+    args = ['--no-default-browser.check']
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+        'chromeOptions' => { 'args' => args}
+    )
+    Capybara.register_driver :selenium do |app|
+        Capybara::Selenium::Driver.new(
+            app,
+            browser: :remote,
+            url: 'http://selenium:4444/wd/hub',
+            desired_capabilities: caps
+        )
+    end
+end
+
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
     config.app_host = 'https://www.americanas.com.br/'
